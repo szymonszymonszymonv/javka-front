2bc844c6ffb4f967ccc9c4358b824c5cfffe1434
szymonszymonszymonv <szymon.prokopiuk99@gmail.com>
2022-06-20 02:31:47 +0200

    push push

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import axiosInstance from './axiosInstance';
+
+const check = () => {
+    axiosInstance.get("/username")
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 function Home() {
+    check()
     return (
         <>
             <h1>Welcome to Java Forum</h1>
