35fda3d887fa6b7b4d320af0017e447ee2e34218
szymonszymonszymonv <szymon.prokopiuk99@gmail.com>
2022-06-22 14:15:53 +0200

    add reports

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import DeletePost from './DeletePost'
+import PostReport from './PostReport'
 import UpdatePost from './UpdatePost'
 
 function PostItem({post}) {
     return (
         <div className="postItem">
             <h3>{post?.user?.username}: {post?.content}</h3>
-            <UpdatePost post={post}/>
+            <UpdatePost post={post} />
             <DeletePost post={post} />
+            <PostReport post={post} />
         </div>
     )
 }
---
+++ src/TopicReport.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+import axiosInstance from './axiosInstance'
+import { useParams } from 'react-router'
+import { useNavigate } from 'react-router'
+
+const sendTopicReport = (topicId) => {
+    console.log("kupcia")
+    axiosInstance.post(`/topics/${topicId}/report`)
+        .then(response => {
+            console.log(response)
+            window.location.reload()
+            // navigate(`/topics/${topicId}`)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+function TopicReport({ topic }) {
+    return (
+        <>
+            <button type="submit" onClick={() => {sendTopicReport(topic.id)}}>Report topic</button>
+        </>
+    )
+}
+
+export default TopicReport
